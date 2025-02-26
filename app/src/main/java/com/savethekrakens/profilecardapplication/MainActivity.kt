43907897cb57bf78e286a0da10952e979668f832
Alexander York <therealalexy@outlook.com>
2025-02-25 21:30:09 -0500

    Adding all files hopefully

---
+++ app/src/main/java/com/savethekrakens/profilecardapplication/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.savethekrakens.profilecardapplication
+
+import android.media.Image
+import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.savethekrakens.profilecardapplication.ui.theme.ProfileCardApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProfileCardApplicationTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                        .background(Color.Yellow),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        ProfilePicture()
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        NameAndBio()
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        val painter = painterResource(R.drawable.little_robot_man_512)
+        Image(
+            painter = painter,
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(228.dp)
+                .clip(CircleShape)
+                .background(Color(0xff4db8ff))
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun NameAndBio() {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "John Doe",
+            modifier = Modifier.padding(8.dp),
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 24.sp
+            )
+        )
+        Text(
+            text = "Android Developer",
+            modifier = Modifier.padding(8.dp),
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProfileCardApplicationTheme {
+        ProfilePicture()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextPreview() {
+    ProfileCardApplicationTheme {
+        NameAndBio()
+    }
+}
