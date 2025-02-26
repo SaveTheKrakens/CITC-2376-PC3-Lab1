5ba01813d973186cfe520b199b6366fa77ed5775
Alexander York <therealalexy@outlook.com>
2025-02-25 21:41:33 -0500

    Optimized Imports for MainActivity.kt

--- app/src/main/java/com/savethekrakens/profilecardapplication/MainActivity.kt
+++ app/src/main/java/com/savethekrakens/profilecardapplication/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.savethekrakens.profilecardapplication
 
-import android.media.Image
 import android.os.Bundle
-import android.provider.ContactsContract.Profile
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,16 +14,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
